95bd780b9617660b733bf5fcb134efa0e719afe5
Guillaume Emerdjian <guillaume.emerdjian@ens.uvsq.fr>
2022-04-19 18:38:35 +0200

    Corrections

--- proies_predateurs.py
+++ proies_predateurs.py
@@ -20,14 +20,13 @@ LARGEUR = 700 # Largeur du canevas
 LARGEUR_CASE = LARGEUR / 1.1 // N
 HAUTEUR_CASE = HAUTEUR / 1.1 // N
 
-
-
 ### Définitions des variables globales
 
 Npro = 10 # Nombre initial de proies (Npro proies apparaissent au début)
 Fpro = 3 # Fréquence de naissance des proies (Fpro proies naissent à chaque tour)
-Apro = 5 # Espérance de vie en nombre de tours
+Apro = 5 # Espérance de vie des proies en nombre de tours
 tour = 0 # Numéro du tour
+
 
 
 ### Définitions des fonctions
@@ -39,33 +38,33 @@ def choix_couleur(n):
         return "green" # Couleur du fond
     else:
         if n[0] == "Proie": # Si c'est une proie
-            return "white" # Couleur des proies
+            return "yellow" # Couleur des proies
         elif n[0] == "Prédateur": # Si c'est un prédateur
             return "red" # Couleur des prédateurs
         else: # Si c'est autre chose
-            return "grey" # Pour mettre les erreurs en évidence on met du gris
+            return "grey" # Couleur pour mettre les erreurs en évidence
 
 
 # Création de la grille
 def init_grille():
     """Retourne une grille carrée vide dimension N+2, les éléments de la configuration vont de 1 à N les indices 0 et N+1 sont les bords et permettent de ne pas gérer de cas particuliers"""
     global grille, config
-    grille = [[0 for i in range(N+2)] for j in range(N+2)]
-    config = [[0 for i in range(N+2)] for j in range(N+2)]
-    for i in range(1, N+1):
+    grille = [[0 for i in range(N + 2)] for j in range(N + 2)]
+    config = [[0 for i in range(N + 2)] for j in range(N + 2)]
+    for i in range(1, N + 1):
         x = (i - 1) * LARGEUR_CASE
-        for j in range(1, N+1):
+        for j in range(1, N + 1):
             y = (j - 1) * HAUTEUR_CASE
             col = "green" # Couleur du fond
-            carre = canvas.create_rectangle(x, y, x + LARGEUR_CASE, y + HAUTEUR_CASE, fill = col, outline = "grey")
+            carre = canvas.create_rectangle(x, y, x + LARGEUR_CASE, y + HAUTEUR_CASE, fill = col, outline = "grey") # outline = couleur du contour des carrés
             grille[i][j] = carre
 
 
 # Affichage de la grille
 def affiche_grille(config):
     """Affiche la configuration donnée"""
-    for i in range(1, N+1):
-        for j in range(1, N+1):
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
             col = choix_couleur(config[i][j])
             canvas.itemconfigure(grille[i][j], fill = col)
 
@@ -93,20 +92,20 @@ def passer_tour():
         for element in range(len(config[ligne])): # Pour chaque élément (ex : ["Proie", 5])
             if config[ligne][element] != 0: # Seulement si c'est pas un 0
                 config[ligne][element][1] -= 1 # Retirer 1 à l'espérance de vie (ex : ["Proie", 5] devient ["Proie", 4])
-                if config[ligne][element][1] == 0: # Si c'est par exemple [Proie, 0], remplacer par 0 
-                    config[ligne][element] = 0
+                if config[ligne][element][1] == 0: # Si c'est par exemple [Proie, 0]
+                    config[ligne][element] = 0 # Remplacer par 0 (mort de l'animal)
     # Ajout de proies
-    cpt = Fpro
+    cpt = Fpro # Compteur égal à Fpro, la fréquence de naissance des proies
     while cpt > 0:
-        i, j = rd.randint(1, N), rd.randint(1, N)
+        i, j = rd.randint(1, N), rd.randint(1, N) # Génération de coordonnées aléatoires
         if config[i][j] == 0: # Si la case est vide
             config[i][j] = ["Proie", Apro] # Création d'une liste avec toutes les infos sur l'animal (ici c'est une proie avec Apro le nombre de tours d'espérance de vie)
             cpt -= 1
-        else: # Si la case n'est pas vide, recommencer la boucle
-            continue
+        else: # Si la case n'est pas vide
+            continue # Recommencer la boucle
     affiche_grille(config)
     tour += 1
-    label_tours.configure(text = ("Tour", tour)) # Actualise le texte du numéro de tour en haut
+    label_tours.configure(text = ("Tour", tour)) # Actualise le texte du numéro de tour
 
 
 
@@ -118,8 +117,8 @@ racine.title("Simulation proies-prédateurs")
 canvas = tk.Canvas(racine, width = LARGEUR, height = HAUTEUR)
 init_grille() # Création de la grille de départ
 init_proies() # Ajout de Npro proies à des coordonnées aléatoires
-bouton_tours = tk.Button(racine, text = "Tour suivant", command = passer_tour)
-label_tours = tk.Label(racine, text = ("Tour", tour))
+bouton_tours = tk.Button(racine, text = "Tour suivant", command = passer_tour) # Bouton pour passer le tour (à remplacer pour ne pas avoir à cliquer sur le bouton pour passer les tours, peut-être avec la commande after())
+label_tours = tk.Label(racine, text = ("Tour", tour)) # Texte pour affiche le numéro du tour actuel
 
 # Placement des widgets
 canvas.grid(column = 1, row = 0, rowspan = 4)
